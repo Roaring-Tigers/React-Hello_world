73e28c29d66b8bdfff484aebed9f0c2d07d3e402
Abhishek Shankar Choudhary <theabhishek.srm@gmail.com>
2023-11-21 12:27:06 +0530

    Hello

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"; 
+import ReactDOM from "react-dom";
+import "./style.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//JSX
+
+const Hello=()=>{
+     let a = 10
+
+     let styleh1 = {textAlign: "center", border: "2px solid green"}
+
+    return (
+        <div className="hello">
+            <h1 style={styleh1}>Hello World, {2+3} </h1>
+            <p> and you happened some it has got activated okay It text to speech has got activated earlier earlier. How to disable it guys I'm not typing once again okay</p>
+        </div>
+    )
+}
+
+
+ReactDOM.render(<Hello />,  document.getElementById("root"))
+
+
+
+
+// Create a react app, which displays your name, Your skills as(ordered List) and your hobbies (unordered list) in a react app
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// functon vs Functional Component
+// function hello(){
+//     return "hello"
+// }
+
+// hello()
+
+// // functional Component
+// function Hello(){
+//     return <h1>Hello</h1>
+// }
+
+{/* <Hello> </Hello>
+<Hello /> */}
